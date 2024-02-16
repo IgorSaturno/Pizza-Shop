28f69ce92a9f40a24a8633d4ec73966a975c4a1b
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2024-02-16 20:55:31 -0300

    Subindo o projeto

---
+++ src/api/update-profile.ts
@@ -0,0 +1,14 @@
+import { api } from '@/lib/axios'
+
+interface UpdateProfileBody {
+  name: string
+  description: string | null
+}
+
+export async function UpdateProfile({ name, description }: UpdateProfileBody) {
+  await new Promise((resolve, reject) => {
+    setTimeout(reject, 3000)
+  })
+
+  await api.put('/profile', { name, description })
+}
